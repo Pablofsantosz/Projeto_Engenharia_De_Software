55900523ca32eecb50b8e4e0efbdb43b098b9115
pablo felipe <pabloletiane@gmail.com>
2025-05-19 18:31:41 -0300

    Funcionalidade aplicacao_excluir funcionando

--- Projeto/aplicacao/views.py
+++ Projeto/aplicacao/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from credenciamento.models import Usuario
+from django.contrib import messages
+from django.contrib.auth import authenticate, logout
+
 
 def aplicacao_homepage(request):
     return render (request,'aplicacao/homepage.html')
 
 @login_required
 def aplicacao_perfil(request):
-    # Garante que o usuário do modelo `Usuario` corresponde ao usuário logado
+    # Garante que o usuário do modelo Usuario corresponde ao usuário logado
     usuario = Usuario.objects.filter(email=request.user.email).first()
 
     # Em casos muito raros, pode não encontrar o usuário no modelo customizado
@@ -19,3 +22,28 @@ def aplicacao_perfil(request):
 
 def aplicacao_excluir(request):
     return render (request,'aplicacao/excluir.html')
+
+# TENTATIVA DE EXCLUIR DO BD
+def aplicacao_excluir(request):
+    if request.method == 'POST':
+        senha = request.POST.get('senha')
+        user = request.user
+        user_auth = authenticate(username=user.username, password=senha)
+
+        if user_auth:
+            try:
+                # Apaga também o modelo customizado
+                Usuario.objects.filter(email=user.email).delete()
+
+                # Apaga o usuário base
+                user.delete()
+
+                logout(request)
+                messages.success(request, 'Sua conta foi excluída com sucesso.')
+                return redirect('login')
+            except Exception as e:
+                messages.error(request, f'Erro ao excluir a conta: {str(e)}')
+        else:
+            messages.error(request, 'Senha incorreta. Tente novamente.')
+
+    return render(request, 'aplicacao/excluir.html')
