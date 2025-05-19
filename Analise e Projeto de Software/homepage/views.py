34f823624c7dc84cbae7f17f5e66d39e27585373
Nunno Wakiyama Diniz Carvalho <142551010+nunnowdc@users.noreply.github.com>
2025-05-19 15:02:08 -0300

    Delete Analise e Projeto de Software directory

--- Analise e Projeto de Software/homepage/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-def welcome_view (request) :
-    return HttpResponse('<h1>Essa é nossa homepage!!<h1/>')
-
-def homepage_paginaInicial(request):
-    return render(request, 'homepage/paginaInicial.html')
--- Analise e Projeto de Software/projetoAPS/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse  # usado para enviar texto de resposta
-
-# função exibir na tela do teste_view
-def teste_view(request):
-    return HttpResponse('Essa é a rota de teste!')
-
-def index_view(request):
-    return HttpResponse('<h1>BEM VINDO!!</h1>')
