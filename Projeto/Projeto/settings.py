8cd1dd86e9d5a53758c235967e2777d0389f00e6
malu-fnb <134156256+malu-fnb@users.noreply.github.com>
2025-06-09 20:09:26 +0000

    feat: adiciona Procfile e .gitignore, ajusta settings.py

--- Projeto/Projeto/settings.py
+++ Projeto/Projeto/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_n-f!o_kk5&&mqn))@^3yd4!ow-1!(oap#5gx+$!%qclm(bh)6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -117,7 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/'"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
